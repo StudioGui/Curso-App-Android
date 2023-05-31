3f2a5496c830f83cd189386ad9fd3c20c4b700e5
Guilherme <contato@studiogui.com.br>
2023-05-31 11:08:42 -0300

    Testes das funções na Controller - MVC (Aula 38)

--- AppListaAlunos/app/src/main/java/devandroid/brandao/applistaalunos/controller/PessoaController.java
+++ AppListaAlunos/app/src/main/java/devandroid/brandao/applistaalunos/controller/PessoaController.java
@@ -1,4 +1,24 @@
 package devandroid.brandao.applistaalunos.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.brandao.applistaalunos.model.Pessoa;
+
 public class PessoaController {
+    @NonNull
+    @Override
+    public String toString() {
+
+        Log.d("MVC_Controller", "Controller Iniciada...");
+
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+
+        Log.d("MVC_Controller", "Salvo: "+pessoa.toString());
+
+    }
 }
